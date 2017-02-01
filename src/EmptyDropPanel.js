fe8655e2dacecd20dd8d39fb22265a06865105a7
dpfaffenbauer <dominik@lineofcode.at>
2017-02-01 11:06:09 +0100

    remove empty message

--- src/EmptyDropPanel.js
+++ src/EmptyDropPanel.js
@@ -2,7 +2,6 @@ Ext.define('DockingPanel.EmptyDropPanel', {
     extend: 'DockingPanel.DropPanel',
     alias: 'widget.emptydroppanel',
 
-    html : 'Drop Here',
     flex : 1,
     title : '',
     draggable : false
